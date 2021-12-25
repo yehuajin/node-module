06106aa680053d6a121ba9e8cfd8514d029f1018
yehj <hjye2@iflytek.com>
2021-12-25 10:22:43 +0800

    初始化项目

---
+++ types.d.ts
@@ -0,0 +1,25 @@
+declare module 'ecstatic';
+// declare module "ecstatic" {
+//   interface opt {
+//     root?: string;
+//     baseDir?: string;
+//     autoIndex?: boolean;
+//     showDir?: boolean;
+//     showDotfiles?: boolean;
+//     humanReadable?: boolean;
+//     hidePermissions?: boolean;
+//     si?: boolean;
+//     cache?: string | number;
+//     cors?: boolean;
+//     gzip?: boolean;
+//     brotli?: boolean;
+//     defaultExt?: "html" | (string & {});
+//     handleError?: boolean;
+//     serverHeader?: boolean;
+//     contentType?: "application/octet-stream" | (string & {});
+//     weakEtags?: boolean;
+//     weakCompare?: boolean;
+//     handleOptionsMethod?: boolean;
+//   }
+//   export default (otions: opt) => any;
+// }
